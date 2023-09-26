6b51b3365e6bf450d87c5bb036c002baf41c8dc6
luisfernandes14 <luis.c.fernandes8@ua.pt>
2023-09-26 10:54:43 +0100

    Exercicios todos aula 1

--- Ex2/test.py
+++ Ex2/test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-maximum_number = 10000
+from colorama import Fore, Back, Style
+#Fore cor da letra, back é o fundo
+maximum_number = 10
 #usar aqui ./test.py | grep "is prime" | wc -l, para descobrir quantos numeros primos tem até 10000
 def isPrime(value):
     for i in range(2,value):
@@ -15,7 +17,7 @@ def main():
 
     for i in range(0, maximum_number): #for cycle to go from 0 to 10
         if isPrime(i):
-            print('Number ' + str(i) + ' is prime.')
+            print('Number ' + Fore.RED + str(i) + Style.RESET_ALL + ' is prime.' )
         else:
             print('Number ' + str(i) + ' is not prime.')
 
